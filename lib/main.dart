5a203fb554fcabed9660a10b17da94ed08d892ab
Alfred Cadawas <alfred.cadawas@gmail.com>
2025-01-14 14:12:41 -0500

    rewrote entire code for nav bar.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:staapp2025/theme/theme.dart';
 import 'package:staapp2025/theme/styles.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,162 +12,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'St Augustie CHS',
       theme: appThemeData,
-      home: const MyHomePage(title: 'St Augustine CHS'),
+      home: MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainScreen> createState() => _MainScreenState();  
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final PageController pageController = PageController(initialPage: 0);
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    CafeteriaMenuPage(),
+    SongRequestsPage(),
+    ProfilePage(),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: pageController,
-        children: const <Widget>[
-          Center(
-            child: Home(),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTap,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Styles.primary,
+        selectedItemColor: Styles.secondary,
+        unselectedItemColor: Styles.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,            
+
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          Center(
-            child: CafeteriaMenu(),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant_menu),
+            label: 'Cafeteria Menu',
           ),
-          Center(
-            child: SongRequests(),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.music_note),
+            label: 'Song Requests',
           ),
-          Center(
-            child: Profile(),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
           ),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Styles.primary,
-            selectedItemColor: Styles.secondary,
-            unselectedItemColor: Styles.white,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,            
-            onTap: (index) {
-              setState(() {
-                pageController.jumpToPage(index);
-              });
-            },
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.local_pizza),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.music_note),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: '',
-              ),
-            ],            
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: Text('Welcome to the Home Page!', style: TextStyle(fontSize: 18)),
         ),
       ),
     );
   }
 }
-class Home extends StatelessWidget {
-    const Home({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World Everybody',
-            ),
-            const Text(
-              'HOME',
-            ),
-          ],
+class CafeteriaMenuPage extends StatelessWidget {
+  const CafeteriaMenuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: Text('Check out our delicious menu!', style: TextStyle(fontSize: 18)),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
 
-class CafeteriaMenu extends StatelessWidget {
-    const CafeteriaMenu({super.key});
+class SongRequestsPage extends StatelessWidget {
+  const SongRequestsPage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World Everybody',
-            ),
-            const Text(
-              'CAFETERIA MENU',
-            ),
-          ],
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: Text('Request your favorite songs!', style: TextStyle(fontSize: 18)),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
 
-class SongRequests extends StatelessWidget {
-    const SongRequests({super.key});
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World Everybody',
-            ),
-            const Text(
-              'SONG REQUESTS',
-            ),
-          ],
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Scaffold(
+        body: Center(
+          child: Text('View and edit your profile.', style: TextStyle(fontSize: 18)),
         ),
-      );
-    }
-}
-
-class Profile extends StatelessWidget {
-    const Profile({super.key});
-
-    @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello World Everybody',
-            ),
-            const Text(
-              'Profile',
-            ),
-          ],
-        ),
-      );
-    }
+      ),
+    );
+  }
 }
 
