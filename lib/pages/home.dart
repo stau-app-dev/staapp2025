629c7247566418eceffa3a58c70e9b0d9474aab5
Alfred Cadawas <alfred.cadawas@gmail.com>
2025-01-15 12:03:40 -0500

    using previous welcome banner on home page as a test

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:staapp2025/theme/styles.dart';
+import 'package:staapp2025/widgets/welcome.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Ignore the OS safearea
+    return SafeArea(
+      top: true,
+      left: true,
+      right: true,
+      bottom: true,
+      // Create a stack and expand the contents?
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          // Create a listview that is scrollable
+          ListView(
+            // Allow Scroll physics
+            physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()
+            ),
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Styles.mainHorizontalPadding),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(height: Styles.mainVerticalPadding),
+                        WelcomeBanner(
+                          dayNumber: 1,
+                          userName: 'Cadawas',
+                        ),
+
+                      ],
+                    ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+
+    
+    );
+
+
+    /*
+    SIMPLE SAMPLE
     return Center(
       child: Scaffold(
         body: Center(
@@ -12,5 +58,6 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+    */
   }
 }
---
+++ lib/widgets/welcome.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:staapp2025/theme/styles.dart';
+import 'package:staapp2025/widgets/reusable/basic_container.dart';
+
+class WelcomeBanner extends StatelessWidget {
+  final int? dayNumber;
+  final String? userName;
+
+  const WelcomeBanner({Key? key, this.dayNumber, this.userName})
+      : super(key: key);
+
+  double getChildWidth(BuildContext context, double percentage) =>
+      MediaQuery.of(context).size.width * percentage - Styles.mainInsidePadding;
+
+  @override
+  Widget build(BuildContext context) {
+    String welcomeText =
+        userName != null ? 'Welcome $userName' : 'Welcome to St. Augustine';
+    String dayNumberText = dayNumber != null ? 'day $dayNumber' : 'day';
+    String dateText = DateFormat('MMMM d, yyyy').format(DateTime.now());
+
+    return BasicContainer(
+        decoration: BoxDecoration(
+            color: Styles.primary,
+            border: Border.all(
+              color: Styles.secondary,
+              width: 3.0,
+            ),
+            borderRadius: Styles.mainBorderRadius,
+            boxShadow: Styles.headerBoxShadow),
+        child: Row(
+          children: [
+            SizedBox(
+                width: getChildWidth(context, 0.5),
+                child: Wrap(
+                  children: [
+                    Text(
+                      welcomeText,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(color: Styles.white),
+                    ),
+                    Container(height: 10.0),
+                    Text('Today is a beautiful $dayNumberText,\n$dateText',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              color: Styles.white,
+                            )),
+                  ],
+                )),
+            const Spacer(),
+            Container(
+                constraints: const BoxConstraints(maxHeight: 200.0),
+                width: getChildWidth(context, 0.3),
+                child: Image.asset('/assets/logos/sta_logo.png',
+                    fit: BoxFit.fitHeight)),
+          ],
+        ));
+  }
+}
