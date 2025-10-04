5a3f998a74994ac5ecdb0be936e5e0f6835f0d8f
Alfred Cadawas <alfred.cadawas@gmail.com>
2025-10-04 00:30:45 +0000

    updated flutter download version

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update -y
 sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 
 # Install Flutter SDK
-FLUTTER_VERSION=3.9.2 # pick the version you use
+FLUTTER_VERSION=3.35.5 # pick the version you use
 curl -LO https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 sudo mv flutter /usr/local/flutter
